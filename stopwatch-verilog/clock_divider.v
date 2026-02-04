1869b2295bb606d806a3521fc7b4ec10cddd22eb
tharunyadav13 <144252939+tharunyadav13@users.noreply.github.com>
2026-02-04 17:35:40 +0530

    clock_divider.v
     clock divider used to convert frequency to time to human understand and  the hardware understanding .

---
+++ stopwatch-verilog/clock_divider.v
@@ -0,0 +1,24 @@
+module clock_divider #(parameter integer clk_freq = 2)
+(
+  input  wire clk,
+  input  wire rst,        // active-low reset
+  output reg  tick_1s
+);
+
+  reg [4:0] count;
+
+  always @(posedge clk or negedge rst) begin
+    if (!rst) begin
+      tick_1s <= 1'b0;
+      count   <= 5'd0;
+    end else begin
+      tick_1s <= 1'b0;                 
+
+      if (count == clk_freq-1) begin   
+        tick_1s <= 1'b1;              
+        count   <= 5'd0;
+      end else begin
+        count <= count + 1'b1;
+      end
+    end
+    endmodule 
