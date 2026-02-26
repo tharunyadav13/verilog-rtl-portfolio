8c14d61dc24d6a8db2025b4d2c2f32228de07ede
tharunyadav13 <144252939+tharunyadav13@users.noreply.github.com>
2026-02-26 12:30:47 +0530

    transaction.sv
    ### Transaction (transaction.sv)
    
    The transaction class models one complete I2C operation.
    
    Fields include:
    - 7-bit slave address
    - Read/Write control bit
    - Register address
    - Data array (for write or read)
    - Expected ACK behavior

---
+++ I2C-PROTOCOL/transaction.sv
@@ -0,0 +1,19 @@
+class transaction;
+
+  rand bit [6:0] addr;
+  rand bit       rw;
+  rand bit [7:0] reg_addr;
+
+  rand int unsigned read_len;
+  rand byte unsigned write_data[$];
+
+  bit addr_ack;
+  bit reg_ack;
+
+  constraint c_addr { addr == 7'h50; }
+  constraint c_reg  { reg_addr == 8'h20; }
+  constraint c_size { write_data.size() == 2; }
+  constraint c_rw   { rw == 1'b0; }
+  constraint c_len  { read_len == 0; }
+
+endclass
