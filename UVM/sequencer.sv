85470335ffc869bd89ecc96ae7c6c276d57a0e9e
tharunyadav13 <144252939+tharunyadav13@users.noreply.github.com>
2026-03-13 16:54:39 +0530

    sequencer.sv

---
+++ UVM/sequencer.sv
@@ -0,0 +1,15 @@
+
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+
+
+class my_sequencer extends uvm_sequencer #(my_transaction);
+
+  `uvm_component_utils(my_sequencer)
+
+  function new(string name = "my_sequencer", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+endclass
